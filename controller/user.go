015cff3d1160eb3c5a4c64ca36a51b63361bf887
dezhizhang <1541609448@qq.com>
2023-08-07 06:13:47 +0800

    feat:集成swagger

--- controller/user.go
+++ controller/user.go
@@ -5,6 +5,18 @@ import (
 	"net/http"
 )
 
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /example/helloworld [get]
+
 func GetUser(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"code": 200, "msg": "获取user成功"})
 }
--- router/router.go
+++ router/router.go
@@ -2,14 +2,19 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"im/controller"
+	"im/docs"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
+	docs.SwaggerInfo.BasePath = ""
 	v1 := r.Group("/api/v1")
 	{
 		v1.GET("/user", controller.GetUser)
 	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	return r
 }
