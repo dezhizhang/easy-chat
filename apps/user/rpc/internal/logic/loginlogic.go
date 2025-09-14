d24149cfbf47ecb1bd08efa2df91312bc934eeb5
dezhizhang <1541609448@qq.com>
2025-09-14 14:19:59 +0800

    feat: 添加登录逻辑

--- apps/user/rpc/internal/logic/loginlogic.go
+++ apps/user/rpc/internal/logic/loginlogic.go
@@ -2,9 +2,12 @@ package logic
 
 import (
 	"context"
-
+	"easy-chat/apps/user/model"
 	"easy-chat/apps/user/rpc/internal/svc"
 	"easy-chat/apps/user/rpc/user"
+	"easy-chat/pkg/auth"
+	"errors"
+	"time"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -26,6 +29,22 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 // Login 登录
 func (l *LoginLogic) Login(in *user.LoginReq) (*user.LoginResp, error) {
 	// todo: add your logic here and delete this line
+	users, err := l.svcCtx.UserModel.FindOneByPhone(l.ctx, in.Phone)
+	if err != nil && !errors.Is(err, model.ErrNotFound) {
+		return nil, errors.New("手机号未注册")
+	}
 
-	return &user.LoginResp{}, nil
+	// 生成token
+	secret := l.svcCtx.Config.Jwt.AccessSecret
+	expire := l.svcCtx.Config.Jwt.AccessExpire
+
+	token, err := auth.GenerateToken(secret, expire, users.Id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now().Unix()
+
+	return &user.LoginResp{Token: token, Expire: int32(now + expire)}, nil
 }
